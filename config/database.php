439803ea987239b3456e7d2d7b1681d9c7b65c3d
MarianaNuri <L24270017@tuxtla.tecnm.mx>
2026-05-18 13:38:16 -0600

    cambio de credenciales

--- config/database.php
+++ config/database.php
@@ -6,10 +6,15 @@ $db = "agenda_app";
 $user = "root";
 $pass = "root"; */
 
-$host = "sql211.infinityfree.com";
-$db = "if0_41931788_agenda";
-$user = "if0_41931788";
-$pass = "GC9RT6QC4F";
+//$host = "sql211.infinityfree.com";
+//$db = "if0_41931788_agenda";
+//$user = "if0_41931788";
+//$pass = "GC9RT6QC4F";
+
+$host = "fdb1032.awardspace.net";
+$db = "4760450_agenda";
+$user = "4760450_agenda";
+$pass = "AgendaContactos1234";
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
