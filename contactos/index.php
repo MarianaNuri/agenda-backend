2811a0cc317cfbaa3cf68752774518dd891aa363
Eva del Carmen Gonzalez Santiz <L24270005@tuxtla.tecnm.mx>
2026-05-18 18:29:04 -0600

    editando index.php

--- contactos/index.php
+++ contactos/index.php
@@ -1,23 +1,28 @@
 <?php
+
+require_once "../config/cors.php";
+
+header("Content-Type: application/json");
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-
 require_once "../config/database.php";
 require_once "../config/auth.php";
-require_once "../config/cors.php";
-
-
 
 $usuario = verificarToken();
 
-$stmt = $conn->prepare("SELECT * FROM contactos WHERE usuario_id = ?");
-$stmt->execute([$usuario['id']]);
+$stmt = $conn->prepare("
+    SELECT *
+    FROM contactos
+    WHERE usuario_id = ?
+");
+
+$stmt->execute([
+    $usuario['id']
+]);
 
 echo json_encode([
     "success" => true,
     "data" => $stmt->fetchAll(PDO::FETCH_ASSOC)
 ]);
-
-//prueba
-?>
